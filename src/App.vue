9ed82ceb158ce4d39fddc319a07c2a05522adcdc
四方 秀 <shu.42111227@gmail.com>
2021-08-19 14:50:19 +0900

    componentのローカル登録

--- src/App.vue
+++ src/App.vue
@@ -3,19 +3,26 @@
     <button @click="changeText">切り替え</button>
     <p v-if="ok">OK！</p>
     <p v-else>Not OK...</p>
-
     <ul>
       <li v-for="fruit in fruits" :key="fruit">{{ fruit }}</li>
     </ul>
+    <p>いいね({{ number }})</p>
+    <LikeNumber />
   </div>
 </template>
 
 <script>
+import LikeNumber from "./LikeNumber";
+
 export default {
+  components: {
+    LikeNumber,
+  },
   data() {
     return {
       ok: true,
       fruits: ["aaa", "bbb", "cccc", "dddd"],
+      number: 5,
     };
   },
   methods: {

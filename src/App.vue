ac33a68ca12e2ba9a0d43d16ad8a68d45adb12bd
四方 秀 <shu.42111227@gmail.com>
2021-08-19 13:05:32 +0900

    v-forのkey属性追加

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
   <div id="app">
+    <button @click="changeText">切り替え</button>
     <p v-if="ok">OK！</p>
+    <p v-else>Not OK...</p>
+
+    <ul>
+      <li v-for="fruit in fruits" :key="fruit">{{ fruit }}</li>
+    </ul>
   </div>
 </template>
 
@@ -9,7 +15,13 @@ export default {
   data() {
     return {
       ok: true,
+      fruits: ["aaa", "bbb", "cccc", "dddd"],
     };
+  },
+  methods: {
+    changeText() {
+      this.ok = !this.ok;
+    },
   },
 };
 </script>
